8a4e35ed843ee8f3adc58849882a4199a94656aa
Yoel Benítez Fonseca <ybenitezf@gmail.com>
2021-07-25 13:21:51 -0400

    feat: :+1: simplificar integración con webpack

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,11 @@
+// Cargar styles
+import "./style.scss";
+
 // Cargar componentes de stimulus
 import "@materializecss/materialize"
 import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
+
 
 const application = Application.start()
 const app_context = require.context("./controllers", true, /\.js$/)
--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -1,12 +1,14 @@
 const autoprefixer = require('autoprefixer');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require('path');
+const devMode = process.env.NODE_ENV !== "production";
 
 function tryResolve_(url, sourceFilename) {
   // Put require.resolve in a try/catch to avoid node-sass failing with cryptic libsass errors
   // when the importer throws
   try {
-    return require.resolve(url, {paths: [path.dirname(sourceFilename)]});
+    return require.resolve(url, { paths: [path.dirname(sourceFilename)] });
   } catch (e) {
     return '';
   }
@@ -23,100 +25,106 @@ function tryResolveScss(url, sourceFilename) {
 function materialImporter(url, prev) {
   if (url.startsWith('@material')) {
     const resolved = tryResolveScss(url, prev);
-    return {file: resolved || url};
+    return { file: resolved || url };
   }
-  return {file: url};
+  return { file: url };
 }
 
 
 module.exports = [{
-    entry: ['./style.scss', './app.js'],
-    output: {
-      path: path.resolve( __dirname, '../newssite/static/assets' ),
-      filename: '[name].js'
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            inject: false,
-            template: './dinamyc.ejs',
-            publicPath: '',
-            filename: path.resolve( __dirname, '../newssite/templates/jsimports.html'),
-        })
-    ],
-    module: {
-      rules: [
-        {
-          test: /\.scss$/,
-          use: [
-            {
-              loader: 'file-loader',
-              options: {
-                name: 'bundle.css',
-              },
-            },
-            { loader: 'extract-loader' },
-            { loader: 'css-loader' },
-            {
-                loader: 'postcss-loader',
-                options: {
-                    postcssOptions: {
-                        plugins: [
-                            autoprefixer()
-                        ]
-                    }
-                }
-            },
-            {
-              loader: 'sass-loader',
-              options: {
-                // Prefer Dart Sass
-                implementation: require('sass'),
-  
-                // See https://github.com/webpack-contrib/sass-loader/issues/804
-                webpackImporter: false,
-                sassOptions: {
-                    importer: materialImporter,
-                    includePaths: ['./node_modules'],
-                    quietDeps: true,
-                },
-              },
-            },
-          ]
-        },
-        {
-            test: /\.js$/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['@babel/preset-env'],
-                    plugins: [
-                      "@babel/plugin-proposal-class-properties"
-                  ]
-                },
+  entry: ['./app.js'],
+  // entry: ['./style.scss', './app.js'],
+  output: {
+    path: path.resolve(__dirname, '../newssite/static/assets'),
+    filename: '[name].[fullhash].js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: false,
+      template: './dinamyc.ejs',
+      publicPath: '',
+      filename: path.resolve(__dirname, '../newssite/templates/webpackbase.html'),
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].[fullhash].css",
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          // {
+          //   loader: 'file-loader',
+          //   options: {
+          //     name: 'bundle.css',
+          //   },
+          // },
+          // { loader: 'extract-loader' },
+          { loader: 'css-loader' },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  autoprefixer()
+                ]
+              }
             }
-        }
-      ]
-    },
-    optimization: {
-        runtimeChunk: 'single',
-        splitChunks: {
-          chunks: 'all',
-          maxInitialRequests: Infinity,
-          minSize: 0,
-          cacheGroups: {
-            vendor: {
-              test: /[\\/]node_modules[\\/]/,
-              name(module) {
-                // get the name. E.g. node_modules/packageName/not/this/part.js
-                // or node_modules/packageName
-                const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
-    
-                // npm package names are URL-safe, but some servers don't like @ symbols
-                return `vendor.${packageName.replace('@', '')}`;
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              // Prefer Dart Sass
+              implementation: require('sass'),
+
+              // See https://github.com/webpack-contrib/sass-loader/issues/804
+              webpackImporter: false,
+              sassOptions: {
+                importer: materialImporter,
+                includePaths: ['./node_modules'],
+                quietDeps: true,
               },
             },
           },
+        ]
+      },
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: [
+              "@babel/plugin-proposal-class-properties"
+            ]
+          },
         }
-    },
-  }];
-  
+      }
+    ]
+  },
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      chunks: 'all',
+      maxInitialRequests: Infinity,
+      minSize: 0,
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name(module) {
+            // get the name. E.g. node_modules/packageName/not/this/part.js
+            // or node_modules/packageName
+            const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+
+            // npm package names are URL-safe, but some servers don't like @ symbols
+            return `vendor.${packageName.replace('@', '')}`;
+          },
+        },
+      },
+    }
+  },
+}];
